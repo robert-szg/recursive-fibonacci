b1167960b9892e56dc2ef0a8d26df6df244e866d
robert-szg <155324734+robert-szg@users.noreply.github.com>
2025-04-15 21:34:14 -0300

    Format code

--- recursive_fibonacci.c
+++ recursive_fibonacci.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int fibonaccir(int n) {
   printf("\nFuncao Inicializada para N = %d, Alocado no Endereco (%p)\n", n, &n);
-  
-	if(n <= 1) {
 
-    if(n == 1) {
+  if (n <= 1) {
+
+    if (n == 1) {
       printf("\n\t\t-----Endereco na Memoria = (%p) | N = 1, retorna %d para a soma\n", &n, n);
     }
 
-    if(n == 0) {
+    if (n == 0) {
       printf("\n\t\t-----Endereco na Memoria = (%p) | N = 0, retorna %d para a soma\n", &n, n);
     }
 
-	  return n;
-	} else {
+    return n;
+  }
+  else {
     printf("\n\t Endereco na Memoria (%p) | %d > 1, chamando a funcao novamente\n\n\n", &n, n);
-		return (fibonaccir(n-1) + fibonaccir(n-2));
-	}
+    return (fibonaccir(n - 1) + fibonaccir(n - 2));
+  }
 }
 
-int main (void) {
+int main(void) {
   int n = 5;
-	printf ("Resultado Final para %d = %d \n", n, fibonaccir(n));
+  printf("Resultado Final para %d = %d \n", n, fibonaccir(n));
   return 0;
 }
